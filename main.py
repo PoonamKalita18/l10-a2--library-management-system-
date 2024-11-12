d28bbeba9a84ed200649bc56ed4cc220148b7337
PoonamKalita18 <arunakalita2015@gmail.com>
2024-11-12 22:01:24 +0530

    xxx

---
+++ main.py
@@ -0,0 +1,49 @@
+class Library :
+    def __init__(self, list_of_books, name):
+        self.bookslist = list_of_books
+        self.name = name
+        self.lenDict ={}
+
+    def displayBooks(self):
+        print("We have the following books: {self.name}")
+        for book in self.booksList:
+            print(book)
+
+    def lendBook(self,user,book):
+            if book not in self.booksList:
+                print("sorry,we don't have this book ")
+            elif book in self.lendDict:
+                ("the book is already being used by {self.lendDict[book]}")
+            else :
+                self.lendDict[book] = user
+                print('lender-book database has been updated. you can take the book now')
+    def addBook(self,book):
+        self.booksList.append(book)
+        print(f"{book} has been dded to the book list")
+    def returnBook(self,book):
+        if book in self.lendDict:
+            del self.lendDict[book]
+            print('book has been returned')
+        else:
+            print("that book wasn't borrowed from us")
+if __name__ == '__main__':
+    books = Library(['Python', 'Rich Dad Poor Dad', 'Harry Potter', 'C++ Basics','Algorithms by CLRS'], "Let's Upskill")
+    user_name = input("Welcome to our library! Please enter your name: ")
+    while True:
+        print(f"\nHello {user_name}, welcome to the {books.name} library. Please choose an option:")
+        print("1. Display Books\n2. Lend a Book\n3. Add a Book\n4. Return a Book\n5. Quit")
+        user_choice = input("Enter your choice to continue: ")
+        if user_choice not in ['1', '2', '3', '4', '5']:
+            print("Please enter a valid option.")
+            continue
+        if user_choice == '1':
+            books.displayBooks()
+        elif user_choice == '2':
+            book = input("Enter the name of the book you want to lend: ")books.lendBook(user_name, book)
+        elif user_choice == '3':
+            book = input("Enter the name of the book you want to add: ")books.addBook(book)
+        elif user_choice == '4':
+            book = input("Enter the name of the book you want to return: ")books.returnBook(book)
+        elif user_choice == '5':
+            print(f"Thank you for using the library, {user_name}. Goodbye!")
+            break
